8362c4a3066de3456b3b1928dd93efbb177da0be
Ahmed Abuelgasim <27422810+ahmed-abuelgasim@users.noreply.github.com>
2023-03-20 23:34:43 -0400

    cleaned up useLocalStorage hook and added basename to BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/notes-app">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
--- src/useLocalStorage.tsx
+++ src/useLocalStorage.tsx
@@ -6,16 +6,9 @@ import { useEffect, useState } from "react";
 export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
 	const [value, setValue] = useState<T>(() => {
 		const jsonValue = localStorage.getItem(key);
-
-		if (jsonValue == null) {
-			if (typeof initialValue == 'function') {
-				return (initialValue as () => T)();
-			} else {
-				return initialValue;
-			}
-		} else {
-			return JSON.parse(jsonValue)
-		}
+		if (jsonValue != null) return JSON.parse(jsonValue)
+		if (typeof initialValue == 'function') return (initialValue as () => T)();
+		return initialValue;
 	});
 
 	useEffect(() => {
