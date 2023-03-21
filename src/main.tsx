2a99611a6c9a00949a1fc94805f7a48e90f68398
Ahmed Abuelgasim <27422810+ahmed-abuelgasim@users.noreply.github.com>
2023-03-20 23:43:58 -0400

    Test use of HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter basename="/notes-app">
+    {/* <BrowserRouter basename="/notes-app"> */}
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>,
 )
